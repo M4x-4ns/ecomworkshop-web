cd2d6a801384bee98a56829c08005ff19fb68157
Deachrit Anusak <deachritans@gmail.com>
2025-05-26 20:36:23 +0700

    deploy

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { use, useEffect } from "react";
-import ProductCard from "../components/card/Productcard";
+import ProductCard from "../components/card/ProductCard";
 import useEcomStore from "../store/ecom-store";
 import SearchCard from "../components/card/SearchCard";
 import CartCard from "../components/card/CartCard";
